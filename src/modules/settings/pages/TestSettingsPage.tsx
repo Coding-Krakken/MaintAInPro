9763aac02b56664f4b4de9c21f6eb57108365446
Coding-Krakken <davidtraversmailbox@gmail.com>
2025-07-18 13:20:26 +0000

    feat: replace placeholder test pages with functional components
    - Updated TestInventoryPage to use InventoryList component
    - Updated TestVendorsPage to use VendorsList component
    - Updated TestEquipmentPage to use EquipmentList component
    - Updated TestSettingsPage to use SettingsList component
    - All pages now show functional interfaces instead of static demo content
    
    This resolves the issue where pages showed 'under development' messages.

---
+++ src/modules/settings/pages/TestSettingsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SettingsList } from '../components/SettingsList';
+
+const TestSettingsPage: React.FC = () => {
+  return <SettingsList />;
+};
+
+export default TestSettingsPage;
