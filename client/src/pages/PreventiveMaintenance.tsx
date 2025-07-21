15ee624b18a9d3d51713d581b32bbd64dd0e8748
Coding-Krakken <davidtraversmailbox@gmail.com>
2025-07-21 19:26:29 +0000

    Initial Branch Commit-Try 2

--- client/src/pages/PreventiveMaintenance.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PMManagement from '@/components/pm/PMManagement';
-
-const PreventiveMaintenance: React.FC = () => {
-  return <PMManagement />;
-};
-
-export default PreventiveMaintenance;
